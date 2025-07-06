2a4f2f113f7efcff0f5ab96dda948a008ca54a85
SSNarvekar04 <salonisachindranathnarvekar@gmail.com>
2025-07-06 18:48:53 +0530

    Add files via upload

---
+++ ProgressBarWrappedIterator.java
@@ -0,0 +1,41 @@
+package me.tongfei.progressbar.wrapped;
+
+import me.tongfei.progressbar.ProgressBar;
+
+import java.util.Iterator;
+import java.util.function.Consumer;
+
+/**
+ * @author Tongfei Chen
+ * @since 0.6.0
+ */
+public class ProgressBarWrappedIterator<T> implements Iterator<T> {
+
+    Iterator<T> underlying;
+    ProgressBar pb;
+
+    public ProgressBarWrappedIterator(Iterator<T> underlying, ProgressBar pb) {
+        this.underlying = underlying;
+        this.pb = pb;
+    }
+
+    @Override
+    public boolean hasNext() {
+        boolean r = underlying.hasNext();
+        if (!r) pb.stop();
+        return r;
+    }
+
+    @Override
+    public T next() {
+        T r = underlying.next();
+        pb.step();
+        return r;
+    }
+
+    @Override
+    public void remove() {
+        underlying.remove();
+    }
+
+}
